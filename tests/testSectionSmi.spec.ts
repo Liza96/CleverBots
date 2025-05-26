e8f7dca783a33a6a9ce8fbe62bee79dee47c559f
Elizaveta <ag1delle@yandex.ru>
2025-05-26 19:29:26 +0300

    Оформление пятого блока тестов

---
+++ tests/testSectionSmi.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+//class="section_x section_x__smi"
+
+test.describe('Группа тестов class section_x section_x__top', () => {
+  test.describe.configure({ timeout: 120_000 });
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://cleverbots.ru/');
+    await page.getByRole('link', { name: 'en EN' }).click({ timeout: 120_000 });
+    await page.getByRole('link', { name: 'ru RU' }).click({ timeout: 50_000 });
+  });
+
+  test('Проверка отображение элемента в home__company__title', async ({ page }) => {
+    await expect(page.getByText('СМИ О НАС')).toBeVisible();
+  });
+
+  test('Проверка наименования элемента в home__company__title', async ({ page }) => {
+    await expect(page.getByText('СМИ О НАС')).toContainText('СМИ О НАС');
+  });
+
+  test('Проверка отображение блока сми', async ({ page }) => {
+    await expect(page.locator('.section_x__smi__wrap').first()).toBeVisible();
+  });
+
+  test('Проверка отображение href элемента в home__company__title', async ({ page }) => {
+    await expect(
+      page.getByRole('group', { name: '1 /' }).getByRole('link').first(),
+    ).toHaveAttribute(
+      'href',
+      'https://www.cnews.ru/news/line/2025-05-13_cleverbots_stereotipysvyazannye',
+    ); //NEWS для примера
+    await expect(page.getByRole('group', { name: '1 /' }).getByRole('link').nth(2)).toHaveAttribute(
+      'href',
+      'https://pro.rbc.ru/news/60a329959a79479e78c4432a',
+    );
+  }); //РБКPRO для примера
+});
